482b7cff058982a1d02d304094d1d85e610cfb1e
Hu Bowen <contact@hubowen.dev>
2024-07-12 15:38:19 +0800

    fix: introduced buffer overflow + css formatting

--- cgi-bin/login.c
+++ cgi-bin/login.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,119 +6,167 @@
 #include <ctype.h>
 #include <unistd.h>
 
-#define LDAP_HOST "ldap://192.168.0.15"  // TODO: Change hard coded IP
+#define LDAP_HOST "ldap://192.168.0.15" // TODO: Change hard coded IP
 #define LDAP_PORT 389
 #define LDAP_BASE_DN "DC=cure51,DC=com"
 
-// Function to URL decode the input
-void url_decode(char *src, char *dest) {
+bool contains_char(char c, char *list)
+{
+    while (*list != '\0')
+    {
+        if (c == *list)
+            return true;
+        list++;
+    }
+
+    return false;
+}
+
+int get_string(char *dest, char *buf, int start, int length, char *breakChar)
+{
+    int cur = start;
+    while (cur < length && !contains_char(buf[cur], breakChar))
+        cur++;
+    strncpy(dest, buf + start, cur - start);
+    dest[cur - start] = '\0';
+    return cur;
+}
+
+void url_decode(char *src, char *dest)
+{
     char *p = src;
     char code[3] = {0};
-    while(*p) {
-        if(*p == '%') {
+    while (*p)
+    {
+        if (*p == '%')
+        {
             memcpy(code, ++p, 2);
             *dest++ = (char)strtoul(code, NULL, 16);
             p += 2;
-        } else if(*p == '+') {
+        }
+        else if (*p == '+')
+        {
             *dest++ = ' ';
             p++;
-        } else {
+        }
+        else
+        {
             *dest++ = *p++;
         }
     }
     *dest = '\0';
 }
 
-// Function to get CGI parameters
-char *get_cgi_param(char *query_string, const char *param) {
-    char *pair;
-    char *value;
-    char *query = strdup(query_string);
-    
-    pair = strtok(query, "&");
-    while(pair) {
-        value = strchr(pair, '=');
-        if(value) {
-            *value = '\0';
-            value++;
-            if(strcmp(pair, param) == 0) {
-                char *result = strdup(value);
-                free(query);
-                return result;
-            }
-        }
-        pair = strtok(NULL, "&");
-    }
-    free(query);
-    return NULL;
+char *html =
+    "\
+    <!DOCTYPE html>\n\
+    <html>\n\
+        <head>\n\
+            <title>Login</title>\n\
+        </head>\n\
+        <body>\n\
+            %s\n\
+        </body>\n\
+    </html>\n";
+
+char *alert_and_return =
+    "\
+    <script>\n\
+        alert(\"%s\");\n\
+        window.location.href = \"/\";\n\
+    </script>\n";
+
+void return_html(char *output)
+{
+    printf("Content-type: text/html\n\n");
+    printf(html, output);
 }
 
-int main() {
+void return_alert(char *output)
+{
+    printf("Content-type: text/html\n\n");
+    printf(alert_and_return, output);
+}
+
+// Login to LDAP with the username & password
+void verify(char *username, char *password)
+{
     LDAP *ld;
     int rc;
-    char *username, *password;
-    char bind_dn[32];
-    char *query_string;
     char decoded_username[256], decoded_password[256];
 
-    // Prepare the response
-    printf("Content-type: text/html\n\n");
-
-    // Get the query string
-    query_string = getenv("QUERY_STRING");
-    if(!query_string || *query_string == '\0') {
-        printf("<html><body><form><p>Username: <input type=\"text\" name=\"username\"></p><p>Password: <input type=\"password\" name=\"password\"></p><p><input type=\"submit\" value=\"Login\"></p></form></body></html>\n");
-        return 1;
-    }
-
-    // Extract username and password from query string
-    username = get_cgi_param(query_string, "username");
-    password = get_cgi_param(query_string, "password");
-
-    // URL decode the username and password
     url_decode(username, decoded_username);
     url_decode(password, decoded_password);
 
-    // Initialize LDAP connection
     rc = ldap_initialize(&ld, LDAP_HOST);
-    if (rc != LDAP_SUCCESS) {
-        printf("Error: ldap_initialize failed: %s\n", ldap_err2string(rc));
-        return 1;
+    if (rc != LDAP_SUCCESS)
+    {
+        fprintf(stderr, "ldap_initialize() failed: %d\n", rc);
+        return_alert("Failed to initialize LDAP");
+        return;
     }
 
-    // Set LDAP version to 3
-    int version = LDAP_VERSION3;
-    ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version);
-
-    // Construct the bind DN vulnerable to buffer overflow
-    strcpy(bind_dn, decoded_username);
-
-    // Perform bind operation
-    rc = ldap_simple_bind_s(ld, bind_dn, decoded_password);
-    if (rc != LDAP_SUCCESS) {
-        printf("<html><body><form><p>Username: <input type=\"text\" name=\"username\"></p><p>Password: <input type=\"password\" name=\"password\"></p><p><input type=\"submit\" value=\"Login\"></p><p style=\"color:red\">Login failed: %s</p></form></body></html>\n", ldap_err2string(rc));
-        ldap_unbind_ext_s(ld, NULL, NULL);
-        return 1;
+    rc = ldap_simple_bind_s(ld, decoded_username, decoded_password);
+    if (rc != LDAP_SUCCESS)
+    {
+        fprintf(stderr, "ldap_simple_bind_s() failed: %d\n", rc);
+        return_alert("Failed to bind to LDAP");
+        return;
     }
 
-    // Unbind and close the connection
-    ldap_unbind_ext_s(ld, NULL, NULL);
+    printf("Content-type: text/html\n\n");
+    printf("<html><body><h1>Welcome, %s</h1><pre>", decoded_username);
 
-    // Overwrite query string with the command to execute
-    snprintf(query_string, strlen(query_string), "./read-files.sh");
-
-    // Print the HTML page displaying the output
-    printf("<html><body><h1>Welcome, %s</h1><p>DEBUG: %s</p><pre>", decoded_username, query_string);
-
-    // Run command and get output
-    FILE *fp = popen(query_string, "r");
+    FILE *fp = popen("/bin/bash ./read-files.sh", "r");
     char buffer[1024];
-    while(fgets(buffer, sizeof(buffer), fp)) {
+    while (fgets(buffer, sizeof(buffer), fp))
+    {
         printf("%s", buffer);
     }
     pclose(fp);
 
     printf("</pre></body></html>\n");
+}
 
+void login()
+{
+    char buf[512];
+    char username[128] = {0};
+    char password[128] = {0};
+    int length;
+
+    gets(buf);
+    length = strlen(buf);
+
+    int start = 0;
+
+    while (start < length)
+    {
+        char data[128];
+        char name[64];
+
+        start = get_string(name, buf, start, length, "=&");
+        if (start >= length || buf[start] == '&')
+        {
+            data[0] = '\0';
+            start++;
+        }
+        else
+        {
+            start = get_string(data, buf, start + 1, length, "&") + 1;
+        }
+
+        if (strcmp(name, "username") == 0)
+            strcpy(username, data);
+        else if (strcmp(name, "password") == 0)
+            strcpy(password, data);
+    }
+
+    verify(username, password);
+}
+
+int main()
+{
+    login();
     return 0;
 }
